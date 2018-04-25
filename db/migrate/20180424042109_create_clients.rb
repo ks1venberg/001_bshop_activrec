4bc776897402fd1f4201b2f50dea4b3b556dfeaf
ks1venberg <venberg.dev@gmail.com>
2018-04-25 06:15:00 +0300

    add t.colums after migration

--- db/migrate/20180424042109_create_clients.rb
+++ db/migrate/20180424042109_create_clients.rb
@@ -1,4 +1,12 @@
 class CreateClients < ActiveRecord::Migration[5.2]
   def change
+  	create_table :client do |t|
+  		t.text :name
+  		t.text :phome
+  		t.text :datestamp
+  		t.text :barber
+  		t.text :color
+
+  		t.timestamps
   end
 end
