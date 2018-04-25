775c83bb118db9723c8700f9125705eb510742e2
ks1venberg <venberg.dev@gmail.com>
2018-04-25 07:35:12 +0300

    db:migrate barbers

---
+++ db/migrate/20180425042025_create_barbers.rb
@@ -0,0 +1,14 @@
+class CreateBarbers < ActiveRecord::Migration[5.2]
+  def change
+
+  	  	create_table :barbers do |t|
+  			t.text :name
+  			t.timestamps
+ 		end
+
+  		Barber.create :name => 'Jessie Pinkman'
+  		Barber.create :name => 'Gus Fring'
+  		Barber.create :name => 'Hisenberg'
+  		Barber.create :name => 'Mike Ehrmanntrout'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_042109) do
+ActiveRecord::Schema.define(version: 2018_04_25_042025) do
 
   create_table "client", force: :cascade do |t|
     t.text "name"
