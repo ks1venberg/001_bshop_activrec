5537848811439cdc8af30a4f8a844c6f38386301
ks1venberg <venberg.dev@gmail.com>
2018-04-26 06:38:16 +0300

    correct migration_barbers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_042025) do
+ActiveRecord::Schema.define(version: 2018_04_26_033228) do
+
+  create_table "barbers", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "client", force: :cascade do |t|
     t.text "name"
